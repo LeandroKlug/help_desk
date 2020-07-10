24517120032229dba51a588e40a7c6ad25d0ba20
LeandroKlug <leandro.softnetwork@gmail.com>
2020-07-10 16:10:27 -0300

    remove files

--- app_help_desk/consultar_chamado.php
+++ app_help_desk/consultar_chamado.php
@@ -22,7 +22,7 @@ fclose($arquivo);
   <head>
     <meta charset="utf-8" />
     <title>App Help Desk</title>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <style>
